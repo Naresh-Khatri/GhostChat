0651d96f46751d3e828ee2a6a41478ecfa135e8c
naresh-khatri <naresh.khatri2345@gmail.com>
2023-02-13 18:33:50 +0530

    wrap inside SimpleCard

--- pages/confess/[user_id].tsx
+++ pages/confess/[user_id].tsx
@@ -1,12 +1,17 @@
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import {
+  Avatar,
+  Box,
   Button,
   Container,
-  Heading,
+  Text,
   Textarea,
   useToast,
+  VStack,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import SimpleCard from '../../components/SimpleCard'
 import supabase from '../../supabase/supabase'
 import { User } from '../../supabase/useUser'
 
@@ -17,7 +22,6 @@ function ConfessPage() {
   const [user, setUser] = useState<User>(null)
   const [error, setError] = useState<any>(null)
   const [confessionText, setConfessionText] = useState<string>('')
-
   const handleConfessSubmitClick = async () => {
     const { status } = await supabase.from('confessions').insert({
       to_user: router.query.user_id,
@@ -48,7 +52,7 @@ function ConfessPage() {
     const fetchUser = async (user_id: string) => {
       const { data, error } = await supabase
         .from('profiles')
-        .select('full_name')
+        .select('full_name , avatar_url')
         .eq('id', user_id)
         .single()
       setUser(data as User)
@@ -79,17 +83,34 @@ function ConfessPage() {
       fetchUser(router.query.user_id as string)
   }, [router, toast])
 
-  if (!user) return null
-  if (error) return null
+  if (!user || error) return null
 
   return (
     <Container>
-      <Heading>Say something to {user.full_name}</Heading>
-      <Textarea
-        value={confessionText}
-        onChange={(e) => setConfessionText(e.target.value)}
-      />
-      <Button onClick={handleConfessSubmitClick}>Submit</Button>
+      <SimpleCard mt='100px'>
+        <Box position='absolute' top={-10} left={'40%'} margin={'auto'}>
+          <Avatar size={'lg'} src={user.avatar_url} />
+        </Box>
+        <VStack mt={6}>
+          <Text>Say something to {user.full_name}</Text>
+          <Textarea
+            value={confessionText}
+            bg={'white'}
+            height={'200px'}
+            color={'black'}
+            resize={'vertical'}
+            onChange={(e) => setConfessionText(e.target.value)}
+          />
+          <Button
+            rightIcon={<ChevronRightIcon />}
+            colorScheme={'blue'}
+            onClick={handleConfessSubmitClick}
+            isDisabled={confessionText.trim().length === 0}
+          >
+            Submit
+          </Button>
+        </VStack>
+      </SimpleCard>
     </Container>
   )
 }
